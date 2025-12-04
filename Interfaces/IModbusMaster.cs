e38bfe24ad64fffa20a2b354bc7857c9b033609f
NNULZH <2465150506@qq.com>
2025-12-04 16:27:50 +0800

    Add files via upload

---
+++ Interfaces/IModbusMaster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IModbusMaster
+    {
+        public void Connect(string host, int port);
+        public void WriteByteAsync(byte b);
+        public Task WriteBytesAsync(byte[] bytes);
+    }
+}
---
+++ Interfaces/ITcpConnnection.cs
@@ -0,0 +1,9 @@
+
+namespace Interfaces
+{
+    public interface ITcpConnnection
+    {
+        void Connect(string host, int port);
+    }
+
+}
