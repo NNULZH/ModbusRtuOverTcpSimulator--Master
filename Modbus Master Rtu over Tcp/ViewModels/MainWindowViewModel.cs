399ce532d5cc9b6bb5616458169d946bd86472f0
NNULZH <2465150506@qq.com>
2025-12-04 16:24:36 +0800

    Add files via upload

---
+++ Modbus Master Rtu over Tcp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Modbus_Master_Rtu_over_Tcp.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
